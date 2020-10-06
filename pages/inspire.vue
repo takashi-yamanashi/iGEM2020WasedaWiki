59a68a5bdd742414ed84b076a61d9a70b8016e17
yamanashi-t <tyamanashi0207@gmail.com>
2020-10-06 17:10:40 +0900

    first commit

---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <v-layout> -->
+    <v-flex class="text-center">
+      <!-- <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      > -->
+      <blockquote class="blockquote">
+        &#8220;TGGGCTTCTGAAGATGCT !.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Takashi Yamanashi</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  <!-- </v-layout> -->
+</template>
