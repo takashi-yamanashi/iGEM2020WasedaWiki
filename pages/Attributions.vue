f9203854acfb972acf7db867348c43888d4b9a84
yamanashi-t <tyamanashi0207@gmail.com>
2020-10-16 14:34:27 +0900

    Attributions Page

---
+++ pages/Attributions.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <v-layout> -->
+    <v-flex class="text-center">
+
+      <blockquote class="blockquote">
+        &#8220;TGGGCTTCTGAAGATGCT !.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;TY</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  <!-- </v-layout> -->
+</template>
