b5a4cc0d390957d87215c4cf82d6900ec50c488a
yamanashi-t <tyamanashi0207@gmail.com>
2020-10-28 05:58:51 +0900

    Monote

---
+++ pages/Protocol.vue
@@ -0,0 +1,302 @@
+<template>
+  <!-- <v-layout> -->
+    <v-container fluid ma-0 pa-0 fill-height> 
+
+        <v-container fluid ma-0 pa-0>
+            <v-row fluid ma-5 pa-5 mb-2  justify="center">
+                <!-- v-if="($vuetify.breakpoint.md||$vuetify.breakpoint.xs||$vuetify.breakpoint.sm)" -->
+                <v-col class="left_box" align="left"  v-if="($vuetify.breakpoint.lg||$vuetify.breakpoint.xl)">
+                    <v-col class="sidebar" ma-0 pa-0>
+
+                        <v-row class="navi_image">
+                               <svg class="navi_icon" z-index='99'>
+                                 <circle fill="#363636" cx="100" cy="100" r="80" stroke="#ffffff" stroke-width="18"/>
+                                <circle fill="none" cx="100" cy="100" r="80" stroke="#008080" stroke-width="18" id="triangle" :style="{ 'stroke-dashoffset': scrollPer}" transform='rotate(270 100 100)'/>  
+                                <!-- style="stroke-dasharray: 420.293; stroke-dashoffset: 210.293;" -->
+                                
+                                Sorry, your browser does not support inline SVG. 
+                              </svg>
+                              <img z-indx="10" class="navi_svg" src="https://2020.igem.org/wiki/images/6/6a/T--Waseda--iGEMprojectIcon.svg">
+            
+                        </v-row>
+
+            
+                    <v-row class="navi_text">
+                        <a href="Protocol#S12"><v-row :class="{ 'title_text': true, 'title_text_active': check[0] }">Cell extract(S12)</v-row></a>
+                        <!-- <a href="Attributions#ModelingTop"><v-row :class="{ 'title_text': true, 'title_text_active': check[1] }">Modeling </v-row></a>
+                        <a href="Attributions#VideoTop"><v-row :class="{ 'title_text': true, 'title_text_active': check[2] }">Video</v-row></a>
+                        <a href="Attributions#WikiTop"><v-row :class="{ 'title_text': true, 'title_text_active': check[3] }">Wiki</v-row></a>
+                        <a href="Attributions#HPTop"><v-row :class="{ 'title_text': true, 'title_text_active': check[4] }">HP</v-row></a>
+                        <a href="Attributions#OtherTop"><v-row :class="{ 'title_text': true, 'title_text_active': check[5] }">Other</v-row></a>
+                         -->
+
+                    </v-row>
+                    </v-col>
+                </v-col>
+
+
+
+                <v-col class="main_text" cols="12" lg="10" xl="10">
+                  
+                    <v-row justify="center" text-align="left">
+                        <v-row cols="12" class='h1_box'>
+                          <v-col class='h1_box_text'> 
+                            <h1 class='h1_text' pushElementOffsetTop>Protocol</h1>
+                          </v-col>
+                        </v-row>
+                    </v-row>
+                    
+                    <v-row v-scroll="pushScrollY" id="OverviewTop"> 
+                      <v-row class="row_max-box">
+                        <h2 class="h2_text">Cell extract(S12) preparation protocol </h2>
+                      </v-row>
+                      </v-row>
+
+                      <v-row cols="12" sm="12" xs="12" md="12" lg="7" xl="7" class="row_max-box">
+
+                        <v-col>
+                          
+                          <p class="p_indent" id="S12"> 
+                              We based the preparation of E. coli extracts for cell-free reactions on a paper by Jewett [Kwon et.al 2015]. The basic protocol is as follows.                            
+                          </P>
+                          <p >
+                                1. Cells were cultured in overnight as pre culture. <br/>
+
+                                2. Diluted overnight cell culture were cultured in fresh medium to OD=3.0 (3~4h) <br/>
+
+                                3. The cells were harvested by centrifuging(7200x g) and washed with S12 Buffer.  <br/>
+
+                                4. Cells were sonicated 1 min 50 sec in total( 900 ~ 1200 J) <br/>
+
+                                5. Lysate were centrifuged. (12000 x g) <br/>
+
+                                6. The supernatant after centrifugation was frozen at -80°C ( keep -80 C)         <br/>                      
+                          </p>
+
+                          <p class="p_indent"> 
+                              S12 Buffer  contained 10 mM Tris-acetate (pH 8.2), 14 mM magnesium acetate, 60 mM potassium glutamate and 2 mM dithiothreitol. 
+
+                                We sonicated cells by sonicator (Sonicator 3000,Qsonia) with microtip(microtip4118, Wakenbtech) 
+                              </p>
+                              <p class="p_indent"> 
+                                  The culture medium and strains used at the time of culture vary from one experiment to another.
+                              </p>
+
+                        </v-col>
+                      </v-row>
+
+                      <v-row cols="12" sm="12" xs="12" md="12" lg="7" xl="7" class="row_max-box">
+
+                        <v-col>
+                          <h3>Reference </h3>
+                          <p>
+                              [Kwon et.al 2015] Kwon, Y., & Jewett, M. C. (2015). High-throughput preparation methods of crude extract for robust cell-free protein synthesis. Scientific Reports, 5(1), 8663. doi:10.1038/srep08663                           
+                          </p>
+                        </v-col>
+                      </v-row>
+
+
+         
+
+
+
+                    <!-- <v-template  v-scroll="pushScrollY" id="section_01"><v-col class="last_message"></v-col></v-template> -->
+                    <!-- <v-template  v-scroll="pushScrollY" id="sectio/n_02"><v-col class="last_message"></v-col></v-template> -->
+                    <v-template  v-scroll="pushScrollY" id="section_03"><v-col class="last_message"></v-col></v-template>
+                    <v-template  v-scroll="pushScrollY" id="section_04"><v-col class="last_message"></v-col></v-template>
+
+ 
+                </v-col>
+            </v-row>
+        </v-container>
+
+    </v-container>
+    
+  <!-- </v-layout> -->
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      fab: false,
+      scrollY: 0,
+      scrollPer: 510.293,
+      sectionOffsetTop: [],
+      check: [false, false, false,],
+
+    };
+  },
+  
+  methods: {
+    pushScrollY(e) {
+      this.scrollY = window.scrollY;
+      for (var i = 0; i < this.check.length; i++) {
+        this.check[i] = false
+      };
+      for (var j=0; j < this.check.length; j++) {
+        if(this.scrollY >= this.sectionOffsetTop[j] && this.scrollY < this.sectionOffsetTop[j+1]) {
+          this.check[j] = true
+        }
+      };
+      const windowHigh = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      this.scrollPer = 510.293*(1 - (this.scrollY/(windowHigh)));
+
+    },
+
+    pushElementOffsetTop() {
+      const targets = [
+        'S12','section_03','section_04',
+      ];
+      targets.forEach(target => {
+        const element = document.getElementById(target);
+        const offsetTop = Math.round(window.scrollY + element.getBoundingClientRect().top);
+        this.sectionOffsetTop.push(offsetTop);
+      });
+    },
+
+
+  },
+  mounted() {
+    this.pushElementOffsetTop();
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+p{
+  font-size: 24px;
+}
+.left_box {
+    padding :0;
+}
+.sidebar {
+    height: 100vh;
+    position: fixed;
+    width: 17%;
+    background-image: url("https://2020.igem.org/wiki/images/e/e9/T--Waseda--Tasukinisakura1.png");
+    background-size: 100% auto;
+    background-repeat: repeat;
+}
+.main_text {
+    padding: 40px 10%;
+    background: rgba(255, 255, 255) 0% 0% no-repeat padding-box;
+}
+.h1 {
+  &_box {
+    border-bottom: 5px solid black;
+    padding-bottom: 0;
+    margin-bottom: 10px;
+    &_text{
+      padding-bottom: 0;
+    }
+  }
+  &_text {
+    font-size: 60px;
+  }
+  &_imbox {
+    display: block;
+    position: relative;
+  }
+  &_image{
+    position: absolute;
+    top: -30px;
+    left: -30px;
+    height: 150px;   
+  }
+}
+h2 {
+  font-size: 30px;
+  border-bottom: 5px solid black;
+  margin: 20px 0;
+}
+
+
+.image_max {
+  width:100%;
+}
+
+.image_top {
+  height: 0;
+  padding-top: 66%;
+  background-size: contain;
+  background-image: url("https://2020.igem.org/wiki/images/6/63/T--Waseda--AttributionsTop.png")
+}
+
+.navi {
+  &_icon {
+    width: 200px;
+    height: 200px;
+  }
+
+  &_image {
+        display: block;
+        position: relative;
+        padding-top: 20px;
+        width: 200px;
+        margin: 0 auto;
+  }
+  &_text {
+        display: block;
+        width: 200px;
+        margin: -40px auto 0 auto;
+        padding: 60px 0 40px 0;
+        border-radius: 20px;
+        background-color: white;
+  }
+  &_svg {
+      position:absolute; top:40px; left:20px; 
+      width: 160px;
+      height: 160px;
+  }
+}
+#triangle {
+  width:160px;
+  height: 160px;
+  stroke-dasharray: 510.293; 
+  // stroke-dashoffset: 420.293 - scrollY;
+}
+
+
+.title_text {
+    display: block;
+    margin: 10px auto;
+    width:60%;
+    border-radius: 10px;
+    background-color: None;
+    &_active{
+      background-color: #507EA4;
+      color: white;
+      opacity: 0.5;
+    }
+}
+.row_max-box {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+.last_message {
+  margin-bottom: 400px;
+}
+
+a:link {
+  color: black;
+  text-decoration: none;
+}
+
+/* visited link */
+a:visited {
+  color: black;
+}
+
+/* mouse over link */
+a:hover {
+  color: hotpink;
+}
+
+/* selected link */
+a:active {
+  color: black;
+}
+</style>
+
